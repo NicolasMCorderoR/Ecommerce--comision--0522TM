7077a18047676aad75908b8464193a2355456cfb
unknown <nicolascordero.227@gmail.com>
2022-12-02 00:10:12 -0300

    segundo commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import Cards from "./components/Cards";
 import Navbar from './components/Navbar';
+import Carousel from "./components/Carousel";
 import "./App.css"
+
 function App() {
   return (
     <div className="App">
-     <Navbar/>
-     
-     
-     <Cards/>
-  
-     
+      <Navbar />
+
+
+      <Cards />
+
+
+      <Carousel />
+
     </div>
   );
 }
